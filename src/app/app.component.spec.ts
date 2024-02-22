03c30f8ea7caeb7780f3f6e77afbe572f0b27f1e
Wonderdann <mylodlyadobrochana@gmail.com>
2024-02-22 21:27:18 +0200

    rename, component templates added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'anki' title`, () => {
+  it(`should have the 'ainki' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('anki');
+    expect(app.title).toEqual('ainki');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, anki');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ainki');
   });
 });
